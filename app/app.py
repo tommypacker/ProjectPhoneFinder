c590d4fb1921e511045f0079dd792ef2ca17a96f
tommypacker <tommypacker@gmail.com>
2016-02-07 13:42:52 -0600

    Edited heroku port

--- app/app.py
+++ app/app.py
@@ -35,4 +35,5 @@ def calling():
 	return render_template('makingCall.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
