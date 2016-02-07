13d2e1fd67f613d82e83c968222ebc59ab767c8a
tommypacker <tommypacker@gmail.com>
2016-02-07 13:46:07 -0600

    Fix port hopefully

--- app/app.py
+++ app/app.py
@@ -35,5 +35,5 @@ def calling():
 	return render_template('makingCall.html')
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
+	port = int(os.environ.get("PORT", 33507))
     app.run(host='0.0.0.0', port=port)
