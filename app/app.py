e3a70154c4fba68dc618c025e7489aa4821c728c
tommypacker <tommypacker@gmail.com>
2016-02-06 22:47:21 -0600

    Added some CSS

--- app/app.py
+++ app/app.py
@@ -13,7 +13,11 @@ def callPhone():
 	phoneNumber = request.form['yourNumber']
 	formattedNum = "+1"+phoneNumber
 	call = client.calls.create(to=formattedNum, from_="+16505138187", url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
-	return redirect('/')
+	return redirect('/making-call/')
+
+@app.route("/making-call/")
+def calling():
+	return render_template('makingCall.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
