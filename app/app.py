c81e61247af5df0eca8a2f93960e127844d21dd7
tommypacker <tommypacker@gmail.com>
2016-02-09 16:09:15 -0600

    Add link to sign up for twilio

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ twilioPhoneNumber = ""
 auth_token = ""
 account_sid = ""
 numberToCall = ""
+entry.Url = "www.twilio.com"
 
 @app.route('/')
 def home():
@@ -43,3 +44,6 @@ def calling():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 33507))
     app.run(host='0.0.0.0', port=port)
+
+
+
